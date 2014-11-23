3f25540b9b921be8032b70170c34c5801c0bcf20
Yuka Ezura <yuka.ezura@gmail.com>
2014-11-23 18:50:29 +0900

    fixed column names of document table

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,20 @@
 class Document < ActiveRecord::Base
+  # version が更新されたら log にデータコピー
+  around_save :save_log
+
+  protected
+    def save_log
+      prev_version = self.previous_version
+      yield
+      log = Log.create(
+        document_id:      self.id,
+        version:          self.previous_version,
+        contents:         self.contents,
+        meta:             self.meta,
+        previous_version: prev_version,
+        next_version:     self.version
+      )
+      # TODO: 保存できなかったときの例外処理
+      logger.debug(log)
+    end
 end
