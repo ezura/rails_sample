cf9b826b7846d3f9887fa69069626c0d34c1cd3a
Yuka Ezura <yuka.ezura@gmail.com>
2014-12-04 19:33:51 +0900

    pass control of document information to javascript

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,12 @@ class Document < ActiveRecord::Base
   # version が更新されたら log にデータコピー
   around_save :save_log
 
+  public
+  #TODO: フレームワーク化するとき残しそう (結局全部 Public になったけど、今後の拡張で変わるかもなので今はこのままで)
+    def to_json_for_public_info
+      self.to_json(:only => [:document_id, :version, :contents, :meta, :previous_version, :next_version])
+    end
+
   protected
     def save_log
       prev_version = self.previous_version
