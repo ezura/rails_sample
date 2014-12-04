bac8c78ff76dfd51bda1171993287c365f21602b
Yuka Ezura <yuka.ezura@gmail.com>
2014-12-04 20:43:25 +0900

    create a class for management document and log model now, only mapping these models.

---
+++ app/assets/javascripts/resource.js
@@ -0,0 +1,24 @@
+"use strict"
+
+var Resource;
+
+// TODO: 過去を指定したときにサーバでリソースを作るか、クライアントにlogを投げて作らせるか
+Resource = function(info) {
+  // FIXME: もっと良い方法ないか
+    this.document_id = ("document_id" in info) ? info.document_id : info.id;
+    this.version = info.version;
+    this.contens = info.contents;
+    this.meta = info.meta;
+    this.previous_version = ("previous_version" in info) ? info.previous_version : null;
+
+    this.next_version = ("next_version" in info) ? next_version : null;
+    this.tmp =  ("tmp" in info) ? info.tmp : null;
+
+    console.log(this);
+}
+
+Resource.prototype = {
+  patch: function(info) {
+    // TODO: log からの情報に従って パッチを当てる (実装によっては進むか戻るかによって処理違なるはず)
+  }
+}
